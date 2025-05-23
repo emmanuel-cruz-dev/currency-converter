88c224ece1b7eb58d8c544ca550d0663d8ddaaee
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-23 12:25:37 -0300

    textMenu agregado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String textMenu = """
+                **********************************************
+                Sea bienvenido/a al Conversor de Moneda =)
+                
+                1) Dólar =>> Peso argentino
+                2) Peso argentino =>> Dólar
+                3) Dólar =>> Real brasileño
+                4) Real brasileño =>> Dólar
+                5) Dólar =>> Peso colombiano
+                6) Peso colombiano =>> Dólar
+                7) Salir
+                Elija una opción válida:
+                **********************************************
+                """;
+
+        System.out.println(textMenu);
     }
 }
